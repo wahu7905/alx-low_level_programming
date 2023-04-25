8ca1c5911aea68851613de9c5c697e7cd60b8594
wahu7905 <wahukibiro@gmail.com>
2023-04-25 15:23:16 -0700

    task 3 and 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * _isalpha - Checks if a character is alphabetic.
  * @c: The character in ASCII code
@@ -7,10 +7,13 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) ||
-	    (c >= 65 && c <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 	_putchar('\n');
 }
