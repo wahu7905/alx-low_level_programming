08b17f0f5f75302461bb777b56cba45a2abac46a
wahu7905 <wahukibiro@gmail.com>
2023-05-04 17:33:25 -0700

    task 9 to 12

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,15 +9,15 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
   /**
-   * main - Entry point
-   * line of code here...
+   * write your line of code here...
    * Remember:
+   * main - magic
    * - not allowed to use a
    * - not allowed to modify p
    * - only one statement
    * - not allowed to code anything else than this line of code
    */
-	p[5] = 98;
+	*(p + 5) = 98;
   /* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
