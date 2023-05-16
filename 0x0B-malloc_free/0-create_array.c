0f4acad0cc8b7349f36e9b9071741510f175e9bb
wahu7905 <wahukibiro@gmail.com>
2023-05-16 13:12:54 -0700

    task 0 to 6

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,10 +2,10 @@
 #include <stdlib>
 
 /**
- * create_array - create array of size and assign char c
+ * create_array - create array of size size and assign char c
  * @size: size of array
  * @c: char to assign
- * Description: create array of size and assign char c
+ * Description: create array of size size and assign char c
  * Return: pointer to array. NULL if fail
  *
  */
